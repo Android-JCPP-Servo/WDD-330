63595dc264d7a6c397708d176e5c8cb721ef7533
Android-JCPP-Servo <astewart1138@gmail.com>
2021-09-22 10:37:38 -0500

    Add exercise from Chapter 2

--- week2/Quiz Ninja/main.js
+++ week2/Quiz Ninja/main.js
@@ -1,1 +1,7 @@
+// Chapter 1 Exercise
 alert('Welcome to Quiz Ninja!');
+
+// Chapter 2 Exercise
+const question = "What is Superman's real name?"
+const answer = prompt(question);
+alert(`You answered ${answer}`);
