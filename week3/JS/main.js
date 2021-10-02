5f282aade7214c8524c14dd3528e7ddd69c05185
Android-JCPP-Servo <astewart1138@gmail.com>
2021-10-01 22:10:09 -0500

    Add remaining examples from Chapter 7.

--- week3/JS/main.js
+++ week3/JS/main.js
@@ -2,4 +2,115 @@ function doSomething(){
     console.log('Something Happened!');
 }
 
+function doSomething(event){
+    console.log(event.type);
+}
+
+function doSomething(event){
+    console.log(event.target);
+}
+
+function doSomething(event){
+    console.log(`screen: (${event.screenX},${event.screenY}), page: (${event.pageX},${event.pageY}), client: (${event.screenX},${event.screenY})`)
+}
+
 addEventListener('click', doSomething);
+
+// Single Click Event
+const clickParagraph = document.getElementById('click');
+
+clickParagraph.addEventListener('click',() => console.log('click') );
+clickParagraph.addEventListener('mousedown',() => console.log('down') );
+clickParagraph.addEventListener('mouseup',() => console.log('up') );
+
+// Double Click Event
+const dblclickParagraph = document.getElementById('dblclick');
+dblclickParagraph.addEventListener('dblclick', highlight);
+
+function highlight(event){
+    event.target.classList.toggle('highlight');
+}
+
+// Mouse Hover Event
+const mouseParagraph = document.getElementById('mouse');
+mouseParagraph.addEventListener('mouseover', highlight);
+mouseParagraph.addEventListener('mouseout', highlight);
+mouseParagraph.addEventListener('mousemove', () =>  console.log('You Moved!') );
+
+// Keyboard Event
+addEventListener('keydown',highlight);
+addEventListener('keyup', (event) => console.log(`You stopped pressing the key on ${new Date}`));
+addEventListener('keypress', (event) => console.log(`You pressed the ${event.key} character`));
+addEventListener('keydown', (event) => console.log(`You pressed the ${event.key} character`));
+addEventListener('keydown', (event) => {
+    if (event.key === 'c' && event.ctrlKey) {
+        console.log('Action canceled!');
+    }
+});
+addEventListener('click', (event) => {
+    if (event.shiftKey) {
+        console.log('A Shifty Click!');
+    }
+});
+
+// Touch Event
+addEventListener('touchend', () => console.log('Touch stopped'));
+
+// Removing Event Listeners
+const onceParagraph = document.getElementById('once');
+onceParagraph.addEventListener('click', remove);
+
+function remove(event) {
+    console.log('Enjoy this while it lasts!');
+    onceParagraph.style.backgroundColor = 'pink';
+    onceParagraph.removeEventListener('click',remove);
+}
+
+// Stopping Default Behavior
+const brokenLink = document.getElementById('broken');
+
+brokenLink.addEventListener('click',(event) => {
+    event.preventDefault();
+    console.log('Broken Link!');
+});
+
+
+// Event Propagation
+ulElement = document.getElementById('list');
+liElement = document.querySelector('#list li');
+
+// Bubbling
+
+ulElement.addEventListener('click', (event) =>
+console.log('Clicked on ul') );
+
+liElement.addEventListener('click', (event) =>
+console.log('Clicked on li') );
+
+// Capturing
+ulElement.addEventListener('click', (event) =>
+console.log('Clicked on ul'),true);
+
+liElement.addEventListener('click', (event) =>
+console.log('Clicked on li'),true);
+
+// Both
+ulElement.addEventListener('click', (event) =>
+console.log('Clicked on ul'),true);
+
+liElement.addEventListener('click', (event) =>
+console.log('Clicked on li'),true);
+
+ulElement.addEventListener('click', (event) =>
+console.log('Clicked on ul'),false );
+
+liElement.addEventListener('click', (event) =>
+console.log('Clicked on li'),false );
+
+// Stopping the Bubbling Phase
+liElement.addEventListener('click', (event) => {
+    console.log('clicked on li');
+    event.stopPropagation(); }, false);
+
+// Event Delegation
+ulElement.addEventListener('click',highlight);
