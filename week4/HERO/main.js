11f3604add6106d1218efb6d009e07565f901301
Android-JCPP-Servo <astewart1138@gmail.com>
2021-10-05 12:43:36 -0500

    Add real name feature to hero.html

--- week4/HERO/main.js
+++ week4/HERO/main.js
@@ -9,6 +9,9 @@ function makeHero(event) {
     event.preventDefault(); // prevent the form from being submitted
     const hero = {}; // create an empty object
     hero.name = form.heroName.value; // create a name property based on the input field's value
+    hero.realName = form.realName.value; // Create a real name property that's displays hidden text
     alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialog
     return hero;
 }
+// Input Fields
+// Text Input Fields
