d830a11ffc7b743aa0111dcbab481b87b6d55ab5
Android-JCPP-Servo <astewart1138@gmail.com>
2021-11-18 21:02:03 -0600

    commit

--- week10/utilities.js
+++ week10/utilities.js
@@ -1,18 +1,19 @@
 export function getJSON(url) {
     return fetch(url)
-    .then(response => {
-        if (response.ok) {
-            console.log("Entered!");
+    .then(function(response) {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        } else {
             return response.json();
-        } throw Error(response.statusText);
+        }
     })
-    .catch(function (err) {
-        console.log("There was an error: " + err);
+    .catch(function(error) {
+        console.log(error);
     });
 }
 
 export const getLocation = function(options) {
-    return new Promise(function (resolve, reject) {
+    return new Promise(function(resolve, reject) {
         navigator.geolocation.getCurrentPosition(resolve, reject, options);
     });
 };
