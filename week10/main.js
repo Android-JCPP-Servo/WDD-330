8373de4555c8a47d109621127f164fcb3a0e5eb5
Android-JCPP-Servo <astewart1138@gmail.com>
2021-11-18 21:14:46 -0600

    commit

--- week10/main.js
+++ week10/main.js
@@ -1,4 +1,23 @@
-import { getJSON, getLocation } from "./utilities.js";
+// import { getJSON, getLocation } from "./utilities.js";
+
+async function getJSON(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw Error(response.statusText);
+        } else {
+            return response.json();
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getLocation = function(options) {
+    return new Promise(function(resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
+};
 
 const baseUrl =
   "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-02-02";
