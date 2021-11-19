cf092cf1addda4d49c4f8f4c0208cf07369040fc
Android-JCPP-Servo <astewart1138@gmail.com>
2021-11-18 21:22:08 -0600

    Make modules

--- week10/main.js
+++ week10/main.js
@@ -1,24 +1,24 @@
-// import { getJSON, getLocation } from "./utilities.js";
+import { getJSON, getLocation } from "./utilities.js";
 
-function getJSON(url) {
-    return fetch(url)
-    .then(function(response) {
-        if (!response.ok) {
-            throw Error(response.statusText);
-        } else {
-            return response.json();
-        }
-    })
-    .catch(function(error) {
-        console.log(error);
-    });
-}
+// function getJSON(url) {
+//     return fetch(url)
+//     .then(function(response) {
+//         if (!response.ok) {
+//             throw Error(response.statusText);
+//         } else {
+//             return response.json();
+//         }
+//     })
+//     .catch(function(error) {
+//         console.log(error);
+//     });
+// }
 
-const getLocation = function(options) {
-    return new Promise(function(resolve, reject) {
-        navigator.geolocation.getCurrentPosition(resolve, reject, options);
-    });
-};
+// const getLocation = function(options) {
+//     return new Promise(function(resolve, reject) {
+//         navigator.geolocation.getCurrentPosition(resolve, reject, options);
+//     });
+// };
 
 const baseUrl =
   "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-02-02";
